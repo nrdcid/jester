c832886786266cc81d8250dcd51d63e41a10fcca
nathandelcid <nade8804@colorado.edu>
2026-07-23 17:29:25 -0500

    sparse autoencoder + loss

--- autoencoder/autoencoder.py
+++ autoencoder/autoencoder.py
@@ -1,13 +1,29 @@
 import torch.nn as nn
 
 class Autoencoder(nn.Module):
-    def __init__(self, encoding_dim):
+    def __init__(self, input_dim, encoding_dim):
         super(Autoencoder, self).__init__()
-        self.encoder = nn.Linear(28 * 28, encoding_dim)
-        self.decoder = nn.Linear(encoding_dim, 28 * 28)
+        self.encoder = nn.Linear(input_dim, encoding_dim)
+        self.decoder = nn.Linear(encoding_dim, input_dim)
 
 
     def forward(self, x):
         x = nn.ReLU(self.encoder(x))
         x = nn.Sigmoid(self.decoder())
         return x
+
+class SparseAutoencoder(Autoencoder):
+    def __init__(self, input_dim, encoding_dim):
+        super(SparseAutoencoder, self).__init__(input_dim, encoding_dim)
+        self.encoder = nn.Linear(input_dim, encoding_dim)
+        self.decoder = nn.Linear(encoding_dim, input_dim)
+
+    def forward(self, x):
+        x = nn.ReLU(self.encoder(x))
+        x = nn.Sigmoid(self.decoder(x))
+        return x
+
+def sparse_loss(x, r, h, lam=0.1):
+    recon_loss = nn.functional.mse_loss(r, x)
+    sparsity_penalty = lam * torch.sum(torch.abs(h))
+    return recon_loss + sparsity_penalty
