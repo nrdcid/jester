e9ef5ac9739342bdd7f9385e6766304543530278
nathandelcid <nade8804@colorado.edu>
2026-08-11 16:40:42 -0500

    refactor: knn

--- knn/helpers.py
+++
@@ -1,5 +0,0 @@
-"""Deprecated shim. Use ``jester.viz`` instead."""
-
-from jester.viz import display_confusion, plot_data, show_decision_surface
-
-__all__ = ["show_decision_surface", "display_confusion", "plot_data"]
